36f7f0a2f740ea63117639a439c98ad858650d2f
hansa-lallu <hansalallu@gmail.com>
2020-04-21 10:08:59 +0100

    fixed some rubocop violations

--- lib/middle_letter.rb
+++ lib/middle_letter.rb
@@ -1,9 +1,9 @@
 class MiddleLetter
   def get_middle(word)
     if word.length.odd?
-      return word[word.length/2]
+      word[word.length/2]
     elsif word.length.even?
-      return word[word.length/2-1] + word[word.length/2]
+      word[word.length/2- 1] + word[word.length/2]
     end 
   end
 end
--- spec/middle_letter_spec.rb
+++ spec/middle_letter_spec.rb
@@ -1,33 +1,30 @@
 require 'middle_letter'
 
 describe MiddleLetter do
-  describe '#get_middle' do 
+  describe '#get_middle' do
     it 'gives the middle letter if word is odd' do
-      word = "testing"
-      expect(subject.get_middle(word)).to eq("t")
-    end 
+      word = 'testing'
+      expect(subject.get_middle(word)).to eq('t')
+    end
 
     it 'gives the middle letter if word is odd' do
-      word = "A"
-      expect(subject.get_middle(word)).to eq("A")
-    end 
-
-    it 'gives the middle letter if word is even' do
-      word = "test"
-      expect(subject.get_middle(word)).to eq("es")
+      word = 'A'
+      expect(subject.get_middle(word)).to eq('A')
     end
 
     it 'gives the middle letter if word is even' do
-      word = "middle"
-      expect(subject.
-        get_middle(word)).to eq("dd")
+      word = 'test'
+      expect(subject.get_middle(word)).to eq('es')
     end
 
     it 'gives the middle letter if word is even' do
-      word = "of"
-      expect(subject.get_middle(word)).to eq("of")
+      word = 'middle'
+      expect(subject.get_middle(word)).to eq('dd')
     end
-  end 
-end 
 
-
+    it 'gives the middle letter if word is even' do
+      word = 'of'
+      expect(subject.get_middle(word)).to eq('of')
+    end
+  end
+end
